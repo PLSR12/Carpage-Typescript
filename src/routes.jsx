f9bd1aed6c108f5ca2979532b006cc27bdd58ae8
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-21 15:04:03 -0300

    create routes for pages using paths

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,22 @@
 import { Switch, Route } from 'react-router-dom'
 
+import Layout from './Layout'
 import Home from './pages/Home'
+import Cars from './pages/Cars'
+import Motors from './pages/Motors'
+import Trucks from './pages/Trucks'
 
-export default function Routes () {
+import paths from './constants/paths'
+
+export default function Routes() {
   return (
     <Switch>
-      <Route path='/' exact component={Home} />
+      <Layout>
+        <Route path={paths.Home} exact component={Home} />
+        <Route path={paths.PageCars} exact component={Cars} />
+        <Route path={paths.PageMotors} exact component={Motors} />
+        <Route path={paths.PageTrucks} exact component={Trucks} />
+      </Layout>
     </Switch>
   )
 }
