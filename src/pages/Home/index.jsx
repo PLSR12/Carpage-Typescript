ef08eb7cb4a65f09bbe86a946fb486da4d666e53
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-21 15:02:35 -0300

    create Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
-export default function Home() {
-    return(
-        <div>
-            <h1>Tey</h1>
-        </div>
-    )
+import { Container, ContainerInfo } from './styles'
+
+export default function Home () {
+  return (
+    <Container>
+      <ContainerInfo>
+        <h1> Sobre Nós </h1>
+        <p>
+          {' '}
+          A Internet e os serviços por ela oferecidos já fazem parte de nossas
+          vidas e vieram para ficar. É sabido que as empresas que não estão
+          presentes na Web de uma forma planejada e eficiente, deixaram de ser
+          competitivas diante da concorrência do mercado globalizado. Sabemos
+          que o modelo de comércio tradicional (de balcão) continuará a existir
+          sempre, mas reflitamos um pouco: no mundo atual, onde a falta de tempo
+          é uma constante na vida de todas as pessoas, a tendência natural do
+          cliente será a de privilegiar cada vez mais quem lhe oferecer produtos
+          e serviços 24 horas por dia, mantendo-o na comodidade e segurança de
+          sua empresa ou de seu lar, livre dos congestionamentos, da violência
+          urbana, das filas e de outros inconvenientes da vida moderna. A
+          Internet proporciona tudo isso e a SuperCarros.com.br quer ampliar
+          ainda mais suas fronteiras digitais. Somos uma equipe altamente
+          qualificada composta por dezenas de profissionais nas áreas de
+          Programação, Design, Prospecção, Atendimento ao Cliente, Web Business,
+          Marketing e Gestão Empresarial. Nosso objetivo é levar a praticidade
+          para dentro de sua casa ou trabalho, para que seus sonhos e projetos
+          se realizem mais rápido e da melhor forma possível.
+        </p>
+      </ContainerInfo>
+    </Container>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,27 @@
 import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: max-content;
+  background-color: white;
+`
+export const ContainerInfo = styled.div`
+  display: flex;
+  background-color: #fff;
+  flex-flow: column wrap;
+  padding-top: 40px;
+  margin: 0 auto;
+
+  h1 {
+    color: rgb(0, 0, 0);
+    font-size: 40px;
+    text-align: center;
+  }
+
+  p {
+    font-size: 20px;
+    color: rgb(0, 0, 0);
+    text-align: justify;
+    padding: 20px 50px 50px 50px;
+  }
+`
