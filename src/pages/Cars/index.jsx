0838f37515d2e61b410c5807c4771e6540db52ee
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-28 21:45:47 -0300

    create CarPage

--- src/pages/Cars/index.jsx
+++ src/pages/Cars/index.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 
-export default function Cars() {
-    return(
-        <div>
-            <h1>Carro</h1>
-        </div>
-    )
+import PropTypes from 'prop-types'
+
+import { Container, BrandsMenu, BrandButton, CarsContainer } from './styles'
+
+import CardCars from '../../components/CardCars'
+
+import formatCurrency from '../../utils/formatCurrency'
+import formatDate from '../../utils/formatDate'
+
+import api from '../../services/api'
+
+export default function Cars({ location: { state } }) {
+  let brandId = 0
+  if (state?.brandId) {
+    brandId = state.brandId
+  }
+
+  const [brands, setBrands] = useState([])
+  const [cars, setCars] = useState([])
+  const [filteredCars, setFilteredCars] = useState([])
+  const [activeBrands, setActiveBrands] = useState(brandId)
+
+  useEffect(() => {
+    async function loadBrands() {
+      const { data } = await api.get('brands')
+
+      let brandsCars = data.slice(0, 6)
+
+      const newBrands = [{ id: 0, name: 'Todas' }, ...brandsCars]
+
+      setBrands(newBrands)
+    }
+    async function loadCars() {
+      const { data: allCars } = await api.get('cars')
+
+      const newCars = allCars.map((car) => {
+        return {
+          ...car,
+          formatedPrice: formatCurrency(car.price),
+          //    formatedDate: formatDate(car.updatedAt)
+        }
+      })
+
+      setCars(newCars)
+    }
+
+    loadCars()
+    loadBrands()
+  }, [])
+
+  useEffect(() => {
+    if (activeBrands === 0) {
+      setFilteredCars(cars)
+    } else {
+      const newFilteredCars = cars.filter(
+        (car) => car.brand_id === activeBrands
+      )
+
+      setFilteredCars(newFilteredCars)
+    }
+  }, [activeBrands, cars])
+
+  return (
+    <Container>
+      <BrandsMenu>
+        {brands &&
+          brands.map((brand) => (
+            <BrandButton
+              type="button"
+              key={brand.id}
+              isActiveBrand={activeBrands === brand.id}
+              onClick={() => {
+                setActiveBrands(brand.id)
+              }}
+            >
+              {brand.name}
+            </BrandButton>
+          ))}
+      </BrandsMenu>
+      <CarsContainer>
+        {filteredCars &&
+          filteredCars.map((car) => <CardCars key={car.id} car={car} />)}
+      </CarsContainer>
+    </Container>
+  )
+}
+
+Cars.propTypes = {
+  location: PropTypes.object,
 }
--- src/pages/Cars/styles.ts
+++ src/pages/Cars/styles.ts
@@ -1,1 +1,41 @@
 import styled from 'styled-components'
+
+type ActiveBrandProps = {
+  border?: any
+  coloring?: any
+}
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #fff;
+`
+
+export const BrandsMenu = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 5vw;
+  margin-top: 5vh;
+`
+
+export const BrandButton = styled.button<ActiveBrandProps>`
+  cursor: pointer;
+  background: none;
+  border: none;
+  border-bottom: ${(props: ActiveBrandProps) =>
+    props.border && '2px solid red'};
+  color: ${(props: ActiveBrandProps) => (props.coloring ? 'red' : '#000')};
+  font-size: 1rem;
+`
+
+export const CarsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 5vw;
+  padding: 3vw;
+`
