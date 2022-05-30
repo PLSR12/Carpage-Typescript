93dd506f15643789ea54249445c13bcbde93d29b
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-29 21:20:43 -0300

    create TrucksPage

--- src/pages/Trucks/index.jsx
+++ src/pages/Trucks/index.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 
-export default function Trucks() {
-    return(
-        <div>
-            <h1>Caminhão</h1>
-        </div>
-    )
+import PropTypes from 'prop-types'
+
+import { Container, BrandsMenu, BrandButton, TrucksContainer } from './styles'
+
+import CardTrucks from '../../components/CardTrucks'
+
+import formatCurrency from '../../utils/formatCurrency'
+
+import api from '../../services/api'
+
+export default function Trucks({ location: { state } }) {
+  let brandId = 0
+  if (state?.brandId) {
+    brandId = state.brandId
+  }
+
+  const [brands, setBrands] = useState([])
+  const [trucks, setTrucks] = useState([])
+  const [filteredTrucks, setFilteredTrucks] = useState([])
+  const [activeBrands, setActiveBrands] = useState(brandId)
+
+  useEffect(() => {
+    async function loadBrands() {
+      const { data } = await api.get('brands')
+
+      let brandsTrucks = data.slice(12, 20)
+
+      const newBrands = [{ id: 0, name: 'Todas' }, ...brandsTrucks]
+
+      setBrands(newBrands)
+    }
+    async function loadTrucks() {
+      const { data: allTrucks } = await api.get('trucks')
+
+      const newTrucks = allTrucks.map((truck) => {
+        return {
+          ...truck,
+          formatedPrice: formatCurrency(truck.price),
+        }
+      })
+
+      setTrucks(newTrucks)
+    }
+
+    loadTrucks()
+    loadBrands()
+  }, [])
+
+  useEffect(() => {
+    if (activeBrands === 0) {
+      setFilteredTrucks(trucks)
+    } else {
+      const newFilteredTrucks = trucks.filter(
+        (truck) => truck.brand_id === activeBrands
+      )
+
+      setFilteredTrucks(newFilteredTrucks)
+    }
+  }, [activeBrands, trucks])
+  return (
+    <Container>
+      <BrandsMenu>
+        {brands &&
+          brands.map((brand) => (
+            <BrandButton
+              type="button"
+              key={brand.id}
+              isActiveBrand={activeBrands === brand.id}
+              onClick={() => {
+                setActiveBrands(brand.id)
+              }}
+            >
+              {brand.name}
+            </BrandButton>
+          ))}
+      </BrandsMenu>
+      <TrucksContainer>
+        {filteredTrucks &&
+          filteredTrucks.map((truck) => (
+            <CardTrucks key={truck.id} truck={truck} />
+          ))}
+      </TrucksContainer>
+    </Container>
+  )
+}
+
+Trucks.propTypes = {
+  location: PropTypes.object,
 }
