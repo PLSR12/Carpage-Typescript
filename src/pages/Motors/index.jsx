4a9fabd07335dcb355b715181dbb5e97c5c29201
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-29 21:20:31 -0300

    create MotorsPages

--- src/pages/Motors/index.jsx
+++ src/pages/Motors/index.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 
-export default function Motors() {
-    return(
-        <div>
-            <h1>Moto</h1>
-        </div>
-    )
+import PropTypes from 'prop-types'
+
+import { Container, BrandsMenu, BrandButton, MotorsContainer } from './styles'
+
+import CardMotorcycles from '../../components/CardMotorcycles'
+
+import formatCurrency from '../../utils/formatCurrency'
+
+import api from '../../services/api'
+
+export default function Motorcycles({ location: { state } }) {
+  let brandId = 0
+  if (state?.brandId) {
+    brandId = state.brandId
+  }
+
+  const [brands, setBrands] = useState([])
+  const [motorcycles, setMotorcycles] = useState([])
+  const [filteredMotorcycles, setFilteredMotorcycles] = useState([])
+  const [activeBrands, setActiveBrands] = useState(brandId)
+
+  useEffect(() => {
+    async function loadBrands() {
+      const { data } = await api.get('brands')
+
+      let brandsMotors = data.slice(6, 12)
+
+      const newBrands = [{ id: 0, name: 'Todas' }, ...brandsMotors]
+
+      setBrands(newBrands)
+    }
+    async function loadMotorcycles() {
+      const { data: allMotorcycles } = await api.get('motors')
+
+      const newMotorcycles = allMotorcycles.map((moto) => {
+        return {
+          ...moto,
+          formatedPrice: formatCurrency(moto.price),
+        }
+      })
+
+      setMotorcycles(newMotorcycles)
+    }
+
+    loadMotorcycles()
+    loadBrands()
+  }, [])
+
+  useEffect(() => {
+    if (activeBrands === 0) {
+      setFilteredMotorcycles(motorcycles)
+    } else {
+      const newFilteredMotorcycles = motorcycles.filter(
+        (moto) => moto.brand_id === activeBrands
+      )
+
+      setFilteredMotorcycles(newFilteredMotorcycles)
+    }
+  }, [activeBrands, motorcycles])
+  return (
+    <Container>
+      <BrandsMenu>
+        {brands &&
+          brands.map((brand) => (
+            <BrandButton
+              type="button"
+              key={brand.id}
+              isActiveBrand={activeBrands === brand.id}
+              onClick={() => {
+                setActiveBrands(brand.id)
+              }}
+            >
+              {brand.name}
+            </BrandButton>
+          ))}
+      </BrandsMenu>
+      <MotorsContainer>
+        {filteredMotorcycles &&
+          filteredMotorcycles.map((moto) => (
+            <CardMotorcycles key={moto.id} moto={moto} />
+          ))}
+      </MotorsContainer>
+    </Container>
+  )
+}
+
+Motorcycles.propTypes = {
+  location: PropTypes.object,
 }
---
+++ src/pages/Motors/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #fff;
+`
+
+export const BrandsMenu = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 5vw;
+  margin-top: 5vh;
+`
+
+export const BrandButton = styled.button`
+  cursor: pointer;
+  background: none;
+  border: none;
+  border-bottom: ${(props) => props.isActiveBrand && '2px solid red'};
+  color: ${(props) => (props.isActiveBrand ? 'red' : '#000')};
+  font-size: 1rem;
+`
+
+export const MotorsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 5vw;
+  padding: 3vw;
+`
