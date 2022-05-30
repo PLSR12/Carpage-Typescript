64d4d096b530a4e87655046f42e20d5a25cb487c
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-29 21:24:23 -0300

    add admin routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,12 +5,17 @@ import Home from './pages/Home'
 import Cars from './pages/Cars'
 import Motors from './pages/Motors'
 import Trucks from './pages/Trucks'
+import Admin from './pages/Admin'
 
 import paths from './constants/paths'
 
 export default function Routes() {
   return (
     <Switch>
+      <Route exact path={paths.Cars} component={Admin} />
+      <Route exact path={paths.NewCar} component={Admin} />
+      <Route exact path={paths.EditCar} component={Admin} />
+      <Route exact path={paths.NewBrand} component={Admin} />
       <Layout>
         <Route path={paths.Home} exact component={Home} />
         <Route path={paths.PageCars} exact component={Cars} />
