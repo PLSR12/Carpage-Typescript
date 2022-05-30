b09dfea2fd41794bb68ab8b56670392f2b53583b
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-29 21:23:29 -0300

    add toastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Routes from './routes'
 import GlobalStyle from './styles/global'
-import Modal from 'react-modal';
+import Modal from 'react-modal'
+import { ToastContainer } from 'react-toastify'
 
 Modal.setAppElement('#root')
 
-export default function App () {
+export default function App() {
   return (
     <>
       <Routes />
+      <ToastContainer autoClose={2000} theme="colored" />
       <GlobalStyle />
     </>
   )
