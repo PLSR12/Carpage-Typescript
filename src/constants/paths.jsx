d034c139d250595258956de090dbb5302895821b
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-29 21:19:10 -0300

    create constants

--- src/constants/paths.jsx
+++ src/constants/paths.jsx
@@ -3,6 +3,20 @@ const paths = {
   PageCars: '/carros',
   PageMotors: '/motos',
   PageTrucks: '/caminhoes',
+  
+  Motors: '/motos/admin',
+  EditMotors: '/motos/editar',
+  NewMotors: '/motos/nova',
+
+  NewBrand: '/nova-marca',
+
+  Cars: '/carros/admin',
+  EditCar: '/carros/editar',
+  NewCar: '/carros/novo',
+
+  Trucks: '/caminhoes/admim',
+  EditTruck: '/caminhoes/editar',
+  NewTruck: '/caminhoes/novo',
 }
 
 export default paths
