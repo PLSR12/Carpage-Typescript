b43a0163d255de3c544a32bb58f4bee6084a6887
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-03 23:40:02 -0300

    changed format for tsx

---
+++ src/pages/Motors/index.tsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+
+import PropTypes from 'prop-types'
+
+import { Container, BrandsMenu, BrandButton, MotorsContainer } from './styles'
+
+import CardMotorcycles from '../../components/CardMotorcycles'
+
+import formatCurrency from '../../utils/formatCurrency'
+
+import api from '../../services/api'
+
+import IVehicles from '../../models/IVehicles'
+
+interface MotoPage {
+  state: any
+  brand: IVehicles
+}
+
+export default function Motorcycles({ brand, state }: MotoPage) {
+  let brandId = 0
+  if (state?.brandId) {
+    brandId = state.brandId
+  }
+
+  const [brands, setBrands] = useState<any>([brandId])
+  const [motorcycles, setMotorcycles] = useState<any>([])
+
+  useEffect(() => {
+    async function loadBrands() {
+      const { data }: any = await api.get('brands')
+
+      let brandsMotors = data.slice(6, 12)
+      console.log(brandsMotors)
+
+      const newBrands = [{ id: 0, name: 'Todas' }, ...brandsMotors]
+
+      setBrands(newBrands)
+    }
+    async function loadMotorcycles() {
+      const { data: allMotorcycles }: any = await api.get('motors')
+
+      const newMotorcycles = allMotorcycles.map(
+        (moto: { price: number | bigint }) => {
+          return {
+            ...moto,
+            formatedPrice: formatCurrency(moto.price),
+          }
+        }
+      )
+
+      setMotorcycles(newMotorcycles)
+    }
+
+    loadMotorcycles()
+    loadBrands()
+  }, [])
+
+  return (
+    <Container>
+      <BrandsMenu>
+        {brands &&
+          brands.map((brand: { id: number; name: string }) => (
+            <BrandButton type="button" key={brand.id}>
+              {brand.name}
+            </BrandButton>
+          ))}
+      </BrandsMenu>
+      <MotorsContainer>
+        {motorcycles &&
+          motorcycles.map((moto: { id: number }) => (
+            <CardMotorcycles key={moto.id} moto={moto} />
+          ))}
+      </MotorsContainer>
+    </Container>
+  )
+}
+
+Motorcycles.propTypes = {
+  location: PropTypes.object,
+}
