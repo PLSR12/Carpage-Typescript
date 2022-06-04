4b06d69715bf0f1e7544111c2121ed52cba0c731
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-04 15:20:09 -0300

    create model for allVehicles

---
+++ src/models/IAllVehicles.d.ts
@@ -0,0 +1,16 @@
+interface IAllVehicles {
+  car: any
+  id: number
+  name: string
+  description: string
+  transmission: string
+  year: number
+  mileage: number
+  fuel: string
+  price: number
+  file: string
+  brandid: string
+  brand: any
+}
+
+export default IAllVehicles
--- src/models/IVehicles.d.ts
+++
@@ -1,6 +0,0 @@
-interface IVehicles{
-    id: number;
-    brand_name: string;
-}
-
-export default IVehicles
