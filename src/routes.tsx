88229d5ccc6fe4f017c851b20b7367924e5db0a1
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-04 15:22:07 -0300

    creates routes for admin

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,17 +5,32 @@ import Home from './pages/Home'
 import Cars from './pages/Cars'
 import Motors from './pages/Motors'
 import Trucks from './pages/Trucks'
-import Admin from './pages/AdminCar'
+import AdminCar from './pages/AdminCar'
+import AdminMotors from './pages/AdminMotor'
+import AdminTrucks from './pages/AdminTruck'
 
 import paths from './constants/paths'
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path={paths.Cars} component={Admin} />
-      <Route exact path={paths.NewCar} component={Admin} />
-      <Route exact path={paths.EditCar} component={Admin} />
-      <Route exact path={paths.NewBrand} component={Admin} />
+      <Route exact path={paths.Cars} component={AdminCar} />
+      <Route exact path={paths.NewCar} component={AdminCar} />
+      <Route exact path={paths.EditCar} component={AdminCar} />
+      <Route exact path={paths.NewBrand} component={AdminCar} />
+      
+      <Route exact path={paths.Motors} component={AdminMotors} />
+      <Route exact path={paths.NewMotor} component={AdminMotors} />
+      <Route exact path={paths.EditMotor} component={AdminMotors} />
+      <Route exact path={paths.NewBrand} component={AdminMotors} />
+
+      
+      <Route exact path={paths.Trucks} component={AdminTrucks} />
+      <Route exact path={paths.NewTruck} component={AdminTrucks} />
+      <Route exact path={paths.EditTruck} component={AdminTrucks} />
+      <Route exact path={paths.NewBrand} component={AdminTrucks} />
+
+
       <Layout>
         <Route path={paths.Home} exact component={Home} />
         <Route path={paths.PageCars} exact component={Cars} />
