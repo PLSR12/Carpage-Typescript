2f0523b60e8a5749ece4694dff8178d0bdec6911
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-04 15:21:29 -0300

    defined props

--- src/pages/Cars/index.tsx
+++ src/pages/Cars/index.tsx
@@ -10,14 +10,7 @@ import formatCurrency from '../../utils/formatCurrency'
 
 import api from '../../services/api'
 
-import IVehicles from '../../models/IVehicles'
-
-interface CarPage {
-  state: any
-  brand: IVehicles
-}
-
-export default function Cars({ brand, state }: CarPage) {
+export default function Cars({ state }: any) {
   let brandId = 0
   if (state?.brandId) {
     brandId = state.brandId
@@ -30,7 +23,6 @@ export default function Cars({ brand, state }: CarPage) {
     async function loadBrands() {
       const { data }: any = await api.get('brands')
       let brandsCars = data.slice(0, 6)
-      console.log(brandsCars)
 
       const newBrands = [{ id: 0, name: 'Todas' }, ...brandsCars]
 
@@ -38,6 +30,8 @@ export default function Cars({ brand, state }: CarPage) {
     }
     async function loadCars() {
       const { data: allCars }: any = await api.get('cars')
+
+      console.log(allCars)
 
       const newCars = allCars.map((car: { price: number | bigint }) => {
         return {
