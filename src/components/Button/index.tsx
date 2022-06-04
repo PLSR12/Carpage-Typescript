8c3d0a3df2310e61d76ac9629cd3ac8cda19f481
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-04 15:17:29 -0300

    add new attribute

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 import { ComponentButton } from './styles'
-import { IButtonProps } from './types'
+import IButtonProps from './types'
 
-export default function Button ({ children, ...props }: IButtonProps) {
-  return <ComponentButton {...props}> {children} </ComponentButton>
+export default function Button({ children, type, ...props }: IButtonProps) {
+  return (
+    <ComponentButton {...props} type="submit">
+      {children}
+    </ComponentButton>
+  )
 }
