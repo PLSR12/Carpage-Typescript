1c379c4710afc84f3c76182d9ff85155635dcb13
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-04 15:15:56 -0300

    add new type

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,5 +1,6 @@
-import { ReactNode } from 'react';
+import { ReactNode } from 'react'
 
-export interface IButtonProps {
-  children: ReactNode;
+export  default interface IButtonProps {
+  children: ReactNode
+  type?: any
 }
