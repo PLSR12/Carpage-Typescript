15bebfeaaf200eb4ea28a25eb71b19985a97ad07
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-03 23:07:09 -0300

    updated

---
+++ src/pages/Trucks/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #fff;
+`
+
+export const BrandsMenu = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 5vw;
+  margin-top: 5vh;
+`
+
+export const BrandButton = styled.button`
+  cursor: pointer;
+  background: none;
+  border: none;
+  font-size: 1rem;
+  line-height: 22px;
+`
+
+export const TrucksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 5vw;
+  padding: 3vw;
+`
