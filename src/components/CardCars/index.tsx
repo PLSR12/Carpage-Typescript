a7d7cc807c225a4ccbcc9f095b34f6089269faf9
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-03 23:04:39 -0300

    changed type for "ICardCarProps"

--- src/components/CardCars/index.tsx
+++ src/components/CardCars/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import PropTypes from 'prop-types'
 
 import Button from '../Button'
@@ -20,7 +18,7 @@ import {
   Line,
 } from './styles'
 
-export default function CardCars({ car }: any) {
+export default function CardCars({ car }: ICardCarsProps) {
   return (
     <ContainerCard>
       <ContainerImage>
